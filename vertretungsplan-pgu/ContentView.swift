e56a4c492d130d740eacb3139166d6db72bacc8f
Jonas Friederichs <wj.friederichs@icloud.com>
2021-09-04 13:58:42 +0200

    Initial Commit

---
+++ vertretungsplan-pgu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vertretungsplan-pgu
+//
+//  Created by Wolf Jonas Friederichs on 9/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vertretungsplan-pgu/vertretungsplan_pguApp.swift
@@ -0,0 +1,17 @@
+//
+//  vertretungsplan_pguApp.swift
+//  vertretungsplan-pgu
+//
+//  Created by Wolf Jonas Friederichs on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct vertretungsplan_pguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
