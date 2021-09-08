f1359dddbed677e12354553882208e893e3591a1
Jonas Friederichs <info@jonasfriederichs.de>
2021-09-08 16:49:29 +0200

    Reorganization of project

---
+++ vertretungsplan-pgu/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Constants
+//
+//  Created by Wolf Jonas Friederichs on 9/8/21.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    let hashedPasswordSchuelerString = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+    
+}
